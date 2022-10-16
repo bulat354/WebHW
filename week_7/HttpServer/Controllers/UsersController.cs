0e5fb5861c8e97abeaa9143bb85ea74446586935
hafizovb <hbulat35@gmail.com>
2022-10-16 16:14:25 +0300

    [add] user controller

--- week_7/HttpServer/Controllers/UsersController.cs
+++ week_7/HttpServer/Controllers/UsersController.cs
@@ -14,13 +14,13 @@ namespace MyServer.Controllers
         [HttpGet("id")]
         public HttpResponse GetUser(int id)
         {
-            if (id < 0 || id >= User.Users.Length)
+            if (id < 0 || id >= User.Users.Count)
                 return HttpResponse.GetNotFoundResponse();
             return new HttpResponse(HttpStatusCode.OK, User.Users[id]);
         }
 
         [HttpGet]
-        public User[] GetUsers()
+        public List<User> GetUsers()
         {
             return User.Users;
         }
@@ -29,6 +29,18 @@ namespace MyServer.Controllers
         public User[] GetUsers(int count)
         {
             return User.Users.Take(count).ToArray();
+        }
+
+        [HttpGet("count")]
+        public int GetCount()
+        {
+            return User.Users.Count;
+        }
+
+        [HttpPost("add")]
+        public void AddUser(int id, string name)
+        {
+            User.Users.Add(new User(id, name));
         }
     }
 
@@ -43,7 +55,7 @@ namespace MyServer.Controllers
             Name = name;
         }
 
-        public static User[] Users { get; set; } = new[]
+        public static List<User> Users { get; set; } = new List<User>()
         {
             new User(0, "Antonio"),
             new User(1, "Montecarlo")
