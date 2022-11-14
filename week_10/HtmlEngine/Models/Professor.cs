827cd2ae155eb2ab38a3fbefd398eae2e0b9e6c8
hafizovb <hbulat35@gmail.com>
2022-11-15 02:56:27 +0300

    Initial Setup

---
+++ week_10/HtmlEngine/Models/Professor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlEngine.Models
+{
+    public class Professor
+    {
+        public string Result { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ week_10/HtmlEngineLibrary/IEngineHtmlService.cs
@@ -0,0 +1,21 @@
+﻿namespace HtmlEngineLibrary
+{
+    public interface IHtmlEngineService
+    {
+        string GetHtml(string template, object model);
+        string GetHtml(Stream template, object model);
+        string GetHtml(byte[] bytes, object model);
+        Stream GetHtmlInStream(string template, object model);
+        Stream GetHtmlInStream(Stream template, object model);
+        Stream GetHtmlInStream(byte[] bytes, object model);
+        byte[] GetHtmlInBytes(string template, object model);
+        byte[] GetHtmlInBytes(Stream template, object model);
+        byte[] GetHtmlInBytes(byte[] bytes, object model);
+        void GenerateAndSaveInDirectory(string outputPath, string outputFileName, string template, object model);
+        void GenerateAndSaveInDirectory(string outputPath, string outputFileName, Stream template, object model);
+        void GenerateAndSaveInDirectory(string outputPath, string outputFileName, byte[] bytes, object model);
+        Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, string template, object model);
+        Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, Stream template, object model);
+        Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, byte[] bytes, object model);
+    }
+}
