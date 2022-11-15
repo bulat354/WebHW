3ef422b3b89edbcbbaaedb672843543c63c58a85
hafizovb <hbulat35@gmail.com>
2022-11-15 03:01:08 +0300

    Add class to generate text and test program

---
+++ week_10/HtmlEngine/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlEngine.Models;
+using HtmlEngineLibrary;
+using HtmlEngineLibrary.TemplateElements;
+using System.Text.RegularExpressions;
+
+namespace HtmlEngine
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var template = @"
+{{ if (Result == ""test1"") begin }} {{Result}}ok
+{{ elseif (Result == ""test2"") continue }} {{Result}}ok
+{{ else continue }} {{Result}} OK
+{{ if end}}
+{{foreach (c in Result) begin }} 
+Character: {{c}}
+{{ foreach end}}";
+            Console.WriteLine(new HtmlEngineService().GetHtml(template, new Professor() { Result = "test1" }));
+        }
+    }
+}
---
+++ week_10/HtmlEngineLibrary/HtmlEngineService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.CodeDom;
+using Microsoft.CSharp;
+
+namespace HtmlEngineLibrary
+{
+    public class HtmlEngineService : IHtmlEngineService
+    {
+
+        public string GetHtml(string template, object model)
+        {
+            var renderer = new TemplateRenderer();
+            return renderer.Render(template, new StatementVariables(model));
+        }
+
+        #region
+        public void GenerateAndSaveInDirectory(string outputPath, string outputFileName, string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateAndSaveInDirectory(string outputPath, string outputFileName, Stream template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateAndSaveInDirectory(string outputPath, string outputFileName, byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, Stream template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GenerateAndSaveInDirectoryAsync(string outputPath, string outputFileName, byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHtml(Stream template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHtml(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHtmlInBytes(string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHtmlInBytes(Stream template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHtmlInBytes(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHtmlInStream(string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHtmlInStream(Stream template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHtmlInStream(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
